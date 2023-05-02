a9d617d0726f2d27803fcf47b1b3754c0868d41c
robtfm <50659922+robtfm@users.noreply.github.com>
2023-05-02 21:01:18 +0100

    restore sceneloop miliis=16

--- src/scene_runner/mod.rs
+++ src/scene_runner/mod.rs
@@ -213,7 +213,7 @@ fn run_scene_loop(world: &mut World) {
     #[cfg(debug_assertions)]
     let millis = 100;
     #[cfg(not(debug_assertions))]
-    let millis = 100;
+    let millis = 16;
     let end_time = last_end_time + Duration::from_millis(millis);
     world.resource_mut::<SceneUpdates>().loop_end_time = end_time;
 
