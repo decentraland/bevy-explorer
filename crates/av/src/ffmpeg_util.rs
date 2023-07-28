91f94ce23e394125a76a41d4fe1e1b9b472332f9
robtfm <50659922+robtfm@users.noreply.github.com>
2023-07-28 13:41:12 +0100

    use thread to reload video

--- crates/av/src/ffmpeg_util.rs
+++ crates/av/src/ffmpeg_util.rs
@@ -1,7 +1,4 @@
-use ffmpeg_next::{
-    format::{context::Input, input},
-    Packet,
-};
+use ffmpeg_next::{format::context::Input, Packet};
 
 pub const BUFFER_TIME: f64 = 10.0;
 
@@ -14,7 +11,8 @@ pub trait PacketIter {
 
 // input stream wrapper allows reloading
 pub struct InputWrapper {
-    input: Input,
+    input: Option<Input>,
+    pending_input: Option<tokio::sync::oneshot::Receiver<Input>>,
     path: String,
     is_eof: bool,
 }
@@ -22,10 +20,48 @@ pub struct InputWrapper {
 impl InputWrapper {
     pub fn new(input: Input, path: String) -> Self {
         Self {
-            input,
+            input: Some(input),
+            pending_input: None,
             path,
             is_eof: false,
         }
+    }
+}
+
+impl InputWrapper {
+    fn get_input(&mut self, blocking: bool) -> Option<&mut Input> {
+        if self.input.is_some() {
+            return Some(self.input.as_mut().unwrap());
+        }
+
+        if blocking {
+            if let Some(pending_input) = self.pending_input.take() {
+                match pending_input.blocking_recv() {
+                    Ok(input) => {
+                        self.input = Some(input);
+                        return Some(self.input.as_mut().unwrap());
+                    }
+                    Err(_) => {
+                        self.is_eof = true;
+                        return None;
+                    }
+                }
+            }
+        } else if let Some(pending_input) = self.pending_input.as_mut() {
+            match pending_input.try_recv() {
+                Ok(input) => {
+                    self.input = Some(input);
+                    return Some(self.input.as_mut().unwrap());
+                }
+                Err(tokio::sync::oneshot::error::TryRecvError::Empty) => return None,
+                Err(tokio::sync::oneshot::error::TryRecvError::Closed) => {
+                    self.is_eof = true;
+                    return None;
+                }
+            }
+        }
+
+        None
     }
 }
 
@@ -35,9 +71,10 @@ impl PacketIter for InputWrapper {
     }
 
     fn try_next(&mut self) -> Option<(usize, Packet)> {
+        let input = self.get_input(false)?;
         let mut packet = Packet::empty();
 
-        match packet.read(&mut self.input) {
+        match packet.read(input) {
             Ok(..) => Some((packet.stream(), packet)),
             Err(ffmpeg_next::util::error::Error::Eof) => {
                 self.is_eof = true;
@@ -48,10 +85,11 @@ impl PacketIter for InputWrapper {
     }
 
     fn blocking_next(&mut self) -> Option<(usize, Packet)> {
+        let input = self.get_input(true)?;
         let mut packet = Packet::empty();
 
         loop {
-            match packet.read(&mut self.input) {
+            match packet.read(input) {
                 Ok(..) => return Some((packet.stream(), packet)),
                 Err(ffmpeg_next::util::error::Error::Eof) => {
                     self.is_eof = true;
@@ -63,11 +101,21 @@ impl PacketIter for InputWrapper {
     }
 
     fn reset(&mut self) {
-        if self.input.seek(0, ..).is_err() {
+        let Some(input) = self.get_input(false) else {
+            return
+        };
+
+        if input.seek(0, ..).is_err() {
             // reload
-            if let Ok(reloaded_input) = input(&self.path) {
-                self.input = reloaded_input;
-            }
+            let (sx, rx) = tokio::sync::oneshot::channel();
+            let path = self.path.clone();
+            std::thread::spawn(move || {
+                if let Ok(input) = ffmpeg_next::format::input(&path) {
+                    let _ = sx.send(input);
+                }
+            });
+            self.input = None;
+            self.pending_input = Some(rx);
         }
 
         self.is_eof = false;
