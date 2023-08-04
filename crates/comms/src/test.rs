a179814db009239e6b54c9296d71980c630f1462
robtfm <50659922+robtfm@users.noreply.github.com>
2023-08-04 16:34:57 +0100

    just reqwest

--- crates/comms/src/test.rs
+++ crates/comms/src/test.rs
@@ -6,15 +6,15 @@ fn test_reqwest_tls() {
     let _ = reqwest::blocking::get("https://www.google.com/").unwrap();
 }
 
-#[test]
-fn test_async_tls() {
-    futures_lite::future::block_on(async move {
-        let remote_address = "wss://sdk-test-scenes.decentraland.zone/mini-comms/room-1";
-        let mut request = remote_address.into_client_request()?;
-        request
-            .headers_mut()
-            .append("Sec-WebSocket-Protocol", HeaderValue::from_static("rfc5"));
-        async_tungstenite::async_std::connect_async(request).await
-    })
-    .unwrap();
-}
+// #[test]
+// fn test_async_tls() {
+//     futures_lite::future::block_on(async move {
+//         let remote_address = "wss://sdk-test-scenes.decentraland.zone/mini-comms/room-1";
+//         let mut request = remote_address.into_client_request()?;
+//         request
+//             .headers_mut()
+//             .append("Sec-WebSocket-Protocol", HeaderValue::from_static("rfc5"));
+//         async_tungstenite::async_std::connect_async(request).await
+//     })
+//     .unwrap();
+// }
