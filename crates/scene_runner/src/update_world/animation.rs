b8a6f89b2d4eb85baebd677eff5bd7cf8c4c9b73
robtfm <50659922+robtfm@users.noreply.github.com>
2023-08-10 13:24:33 +0100

    play first anim if no animator present

--- crates/scene_runner/src/update_world/animation.rs
+++ crates/scene_runner/src/update_world/animation.rs
@@ -6,7 +6,10 @@ use bevy::prelude::*;
 
 use common::sets::SceneSets;
 use dcl::interface::ComponentPosition;
-use dcl_component::{proto_components::sdk::components::PbAnimator, SceneComponentId};
+use dcl_component::{
+    proto_components::sdk::components::{PbAnimationState, PbAnimator},
+    SceneComponentId,
+};
 
 use super::{gltf_container::GltfProcessed, AddCrdtInterfaceExt};
 
@@ -41,52 +44,76 @@ impl From<PbAnimator> for Animator {
 #[allow(clippy::type_complexity)]
 fn update_animations(
     mut animators: Query<
-        (&mut Animator, &Handle<Gltf>, &GltfProcessed),
+        (Option<&mut Animator>, &Handle<Gltf>, &mut GltfProcessed),
         Or<(Changed<Animator>, Changed<GltfProcessed>)>,
     >,
     mut players: Query<&mut AnimationPlayer, &Name>,
     clips: Res<Assets<AnimationClip>>,
     gltfs: Res<Assets<Gltf>>,
 ) {
-    for (mut animator, h_gltf, gltf_processed) in animators.iter_mut() {
-        // TODO bevy only supports a single concurrent animation (or a single timed transition which we can't use)
-        // it is still in development so will probably have better support soon. otherwise we could build our own
-        // animator to handle blending if required.
-        // for now, we choose highest weighted animation
-        let (_, req_state) = animator
-            .pb_animator
-            .states
-            .iter()
-            .fold((0.0, None), |v, state| {
-                if !state.playing.unwrap_or_default() {
-                    return v;
-                }
+    for (mut maybe_animator, h_gltf, mut gltf_processed) in animators.iter_mut() {
+        let maybe_h_clip = match maybe_animator {
+            Some(ref animator) => {
+                // TODO bevy only supports a single concurrent animation (or a single timed transition which we can't use)
+                // it is still in development so will probably have better support soon. otherwise we could build our own
+                // animator to handle blending if required.
+                // for now, we choose highest weighted animation
+                let (_, req_state) =
+                    animator
+                        .pb_animator
+                        .states
+                        .iter()
+                        .fold((0.0, None), |v, state| {
+                            if !state.playing.unwrap_or_default() {
+                                return v;
+                            }
 
-                let current_weight = v.0;
-                let state_weight = state.weight.unwrap_or(1.0);
-                if state_weight > current_weight {
-                    (state_weight, Some(state))
+                            let current_weight = v.0;
+                            let state_weight = state.weight.unwrap_or(1.0);
+                            if state_weight > current_weight {
+                                (state_weight, Some(state))
+                            } else {
+                                v
+                            }
+                        });
+
+                if let Some(state) = req_state {
+                    let Some(gltf) = gltfs.get(h_gltf) else {
+                        // set change tick on the animator so that we recheck next frame
+                        // TODO this will recheck forever if the gltf fails to load
+                        gltf_processed.set_changed();
+                        continue;
+                    };
+
+                    let Some(h_clip) = gltf.named_animations.get(&state.clip) else {
+                        warn!("requested clip {} doesn't exist", state.clip);
+                        continue;
+                    };
+                    Some((h_clip, state.clone()))
                 } else {
-                    v
+                    None
                 }
-            });
+            }
+            None => {
+                // if no animator is present we should play the first clip, if any exist
+                let Some(gltf) = gltfs.get(h_gltf) else {
+                    // set change tick on the animator so that we recheck next frame
+                    // TODO this will recheck forever if the gltf fails to load
+                    gltf_processed.set_changed();
+                    continue;
+                };
 
-        if let Some(state) = req_state {
-            let Some(gltf) = gltfs.get(h_gltf) else {
-                // set change tick on the animator so that we recheck next frame
-                // TODO this will recheck forever if the gltf fails to load
-                animator.set_changed();
-                continue;
-            };
+                gltf.animations
+                    .first()
+                    .map(|anim| (anim, PbAnimationState::default()))
+            }
+        };
 
-            let Some(h_clip) = gltf.named_animations.get(&state.clip) else {
-                warn!("requested clip {} doesn't exist", state.clip);
-                continue;
-            };
+        if let Some((h_clip, state)) = maybe_h_clip {
             let Some(clip) = clips.get(h_clip) else {
                 // set change tick on the animator so that we recheck next frame
                 // TODO this will recheck forever if the gltf fails to load
-                animator.set_changed();
+                gltf_processed.set_changed();
                 continue;
             };
 
@@ -97,11 +124,18 @@ fn update_animations(
             let (target, others) = gltf_processed
                 .animation_roots
                 .iter()
-                .partition::<Vec::<_>, _>(|(_, name)| clip.compatible_with(name));
+                .partition::<Vec<_>, _>(|(_, name)| clip.compatible_with(name));
 
             if target.is_empty() {
                 warn!("invalid root node for animation: {:?}", state.name);
-                warn!("available root nodes: {:?}", gltf_processed.animation_roots.iter().map(|(_, name)| name.as_str()).collect::<Vec<_>>());
+                warn!(
+                    "available root nodes: {:?}",
+                    gltf_processed
+                        .animation_roots
+                        .iter()
+                        .map(|(_, name)| name.as_str())
+                        .collect::<Vec<_>>()
+                );
             }
 
             for (player_ent, _) in target {
@@ -121,9 +155,16 @@ fn update_animations(
                 player.pause();
             }
 
-            animator.bypass_change_detection().playing = true;
-        } else if animator.playing {
-            animator.bypass_change_detection().playing = false;
+            if let Some(animator) = maybe_animator.as_mut() {
+                animator.bypass_change_detection().playing = true;
+            }
+        } else if maybe_animator
+            .as_ref()
+            .map_or(false, |animator| animator.playing)
+        {
+            if let Some(animator) = maybe_animator.as_mut() {
+                animator.bypass_change_detection().playing = false;
+            }
             for (player_ent, _) in gltf_processed.animation_roots.iter() {
                 let mut player = players.get_mut(*player_ent).unwrap();
 
