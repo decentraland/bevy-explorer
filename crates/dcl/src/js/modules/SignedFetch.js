cba6f960019cc8dd901f378f9d1b21ab92fede72
robtfm <50659922+robtfm@users.noreply.github.com>
2023-09-20 17:06:38 +0100

    js - fix signedFetch::getHeaders, add clearTimeout

--- crates/dcl/src/js/modules/SignedFetch.js
+++ crates/dcl/src/js/modules/SignedFetch.js
@@ -26,5 +26,7 @@ module.exports.signedFetch = async function (body) {
 }
 
 module.exports.getHeaders = async function (body) { 
-    return await Deno.core.ops.op_signed_fetch_headers(body.url, body.init?.method);
+    return {
+        headers: await Deno.core.ops.op_signed_fetch_headers(body.url, body.init?.method)
+    };
 }
