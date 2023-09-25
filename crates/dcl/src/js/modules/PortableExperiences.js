6875bada8c327e54a2bf69b6a8f7a6ac203c6078
robtfm <50659922+robtfm@users.noreply.github.com>
2023-09-25 17:52:40 +0100

    debug portables

--- crates/dcl/src/js/modules/PortableExperiences.js
+++ crates/dcl/src/js/modules/PortableExperiences.js
@@ -1,4 +1,16 @@
-module.exports.spawn = async function (body) { return {} }
-module.exports.kill = async function (body) { return {} }
-module.exports.exit = async function (body) { return {} }
-module.exports.getPortableExperiencesLoaded = async function (body) { return {} }
+module.exports.spawn = async function (body) { 
+    console.log("PortableExperiences.spawn", body)
+    return {} 
+}
+module.exports.kill = async function (body) { 
+    console.log("PortableExperiences.kill", body)
+    return {} 
+}
+module.exports.exit = async function (body) { 
+    console.log("PortableExperiences.exit", body)
+    return {} 
+}
+module.exports.getPortableExperiencesLoaded = async function (body) { 
+    console.log("PortableExperiences.getPortableExperiencesLoaded", body)
+    return {} 
+}
