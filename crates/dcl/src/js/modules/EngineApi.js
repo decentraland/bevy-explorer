c064e937abf76580c6640dd585c8c4485e8ad318
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-10-02 14:27:06 -0300

    fix: engine api with sdk6 deprecated apis, and add isServer

--- crates/dcl/src/js/modules/EngineApi.js
+++ crates/dcl/src/js/modules/EngineApi.js
@@ -7,11 +7,6 @@ module.exports.crdtSendToRenderer = async function(messages) {
     };
 }
 
-module.exports.sendBatch = async function() {
-    // console.error("EngineApi::sendBatch not implemented");
-    return { events: [] }
-}
-
 module.exports.crdtGetState = async function() {
     const data = (await Deno.core.opAsync("op_crdt_recv_from_renderer")).map((item) => new Uint8Array(item))
 
@@ -20,3 +15,29 @@ module.exports.crdtGetState = async function() {
     };
 }
 
+module.exports.isServer = async function() {
+    return {
+        isServer: false
+    }
+}
+
+/**
+ * @deprecated this is an SDK6 API.
+ * This function subscribe to an event from the renderer
+ */
+module.exports.subscribe = async function(message) {}
+
+/**
+ * @deprecated this is an SDK6 API.
+ * This function unsubscribe to an event from the renderer
+ */
+module.exports.unsubscribe = async function(message) {}
+
+/**
+ * @deprecated this is an SDK6 API.
+ * This function polls events from the renderer
+ */
+module.exports.sendBatch = async function() {
+    // console.error("EngineApi::sendBatch not implemented");
+    return { events: [] }
+}
