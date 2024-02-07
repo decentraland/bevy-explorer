05f7d14cdaf509c970d403444030061460cae35c
robtfm <50659922+robtfm@users.noreply.github.com>
2024-02-07 10:33:06 +0000

    fix tests

--- crates/ipfs/src/lib.rs
+++ crates/ipfs/src/lib.rs
@@ -402,9 +402,9 @@ impl Plugin for IpfsIoPlugin {
     fn build(&self, app: &mut App) {
         info!("remote server: {:?}", self.starting_realm);
 
-        let file_path = self.cache_root.clone().unwrap_or("assets".to_owned());
+        let file_path = self.cache_root.clone().unwrap_or("assets/cache/".to_owned());
         let default_reader = FileAssetReader::new(file_path.clone());
-        let cache_root = default_reader.root_path().to_owned().join("cache/");
+        let cache_root = default_reader.root_path().to_owned();
         std::fs::create_dir_all(&cache_root).expect("failed to write to assets folder");
 
         let static_paths = HashMap::from_iter([("genesis_tx.png", "images/genesis_tx.png")]);
