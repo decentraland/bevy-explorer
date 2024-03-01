5cf1555780e7f6e0cdb004bdf2318f0e3d921d53
robtfm <50659922+robtfm@users.noreply.github.com>
2024-03-01 00:21:05 +0000

    fix world ui size calc on old nodes

--- crates/world_ui/src/lib.rs
+++ crates/world_ui/src/lib.rs
@@ -71,7 +71,7 @@ fn update_world_ui(
     quad_query: Query<&Handle<TextShapeMaterial>>,
     mut images: ResMut<Assets<Image>>,
     mut uis: Query<&mut Visibility>,
-    mut current_rendered_ui: Local<Option<Entity>>,
+    mut current_rendered_ui: Local<Option<(Entity, usize)>>,
 ) {
     // remove old quads
     for e in removed.read() {
@@ -118,139 +118,152 @@ fn update_world_ui(
         return;
     };
 
-    cam.is_active = false;
-
     let mut new_uis = q.iter();
-
-    // run one thing per frame as we reuse the camera
-    if let Some((ent, ui)) = new_uis.next() {
-        if let Some(ent) = current_rendered_ui.take() {
-            if let Ok(mut vis) = uis.get_mut(ent) {
-                *vis = Visibility::Hidden;
-            }
+        
+    match current_rendered_ui.as_mut() {
+        Some((_, countdown)) if *countdown > 0 => {
+            *countdown -= 1;
         }
+        _ => {
+            cam.is_active = false;
 
-        if let Ok(mut vis) = uis.get_mut(ui.ui_root) {
-            *vis = Visibility::Visible;
-        }
-
-        let image_size = Extent3d {
-            width: if ui.resize_width.is_some() {
-                16
-            } else {
-                ui.width.max(16)
-            },
-            height: if ui.resize_height.is_some() {
-                16
-            } else {
-                ui.height.max(16)
-            },
-            depth_or_array_layers: 1,
-        };
-
-        let material_data = TextQuadData {
-            valign: ui.valign,
-            halign: ui.halign,
-            pix_per_m: ui.pix_per_m,
-            add_y_pix: ui.add_y_pix,
-        };
-
-        // update camera
-        cam.is_active = true;
-        target.width = ui.resize_width;
-        target.height = ui.resize_height;
-        target.info.max_width = Some(ui.width);
-        target.info.max_height = Some(ui.height);
-
-        // create or update camera and quad
-        let (quad, image) = if let Some(prev_items) = wui.lookup.get(&ent) {
-            if let Ok(quad) = quad_query.get(prev_items.quad) {
-                if let Some(mat) = materials.get_mut(quad) {
-                    // update valign
-                    mat.extension.data = material_data;
-
-                    // update image
-                    if let Some(image) =
-                        images.get_mut(mat.base.base.base_color_texture.clone().unwrap())
-                    {
-                        let current_size = image.size();
-
-                        let width_ok =
-                            ui.resize_width.is_some() || image_size.width == current_size.x;
-                        let height_ok =
-                            ui.resize_height.is_some() || image_size.height == current_size.y;
-                        if !width_ok || !height_ok {
-                            image.resize(image_size);
-                        }
-                    }
+            if let Some((last_ent, _)) = current_rendered_ui.take() {
+                if let Some(mut commands) = commands.get_entity(last_ent) {
+                    commands.remove::<TargetCamera>();
+                }
+                if let Ok(mut vis) = uis.get_mut(last_ent) {
+                    *vis = Visibility::Hidden;
                 }
             }
+    
+            // run one thing per frame as we reuse the camera
+            if let Some((ent, ui)) = new_uis.next() {
+                debug!("wui render {ent:?}");
 
-            // dispose of previous ui if required
-            if let Some(commands) = prev_items.ui.and_then(|e| commands.get_entity(e)) {
-                commands.despawn_recursive();
-            }
-
-            (prev_items.quad, prev_items.image.clone())
-        } else {
-            // create render target image (it'll be resized)
-            let mut image = Image::new_fill(
-                image_size,
-                TextureDimension::D2,
-                &[0, 0, 0, 0],
-                TextureFormat::Bgra8UnormSrgb,
-            );
-            image.texture_descriptor.usage |= TextureUsages::RENDER_ATTACHMENT;
-            let image = images.add(image);
-
-            let quad = commands
-                .spawn((
-                    MaterialMeshBundle {
-                        mesh: meshes.add(shape::Quad::default().into()),
-                        material: materials.add(TextShapeMaterial {
-                            base: SceneMaterial {
-                                base: StandardMaterial {
-                                    base_color_texture: Some(image.clone()),
-                                    unlit: true,
-                                    double_sided: true,
-                                    cull_mode: None,
-                                    alpha_mode: AlphaMode::Mask(0.5),
-                                    ..Default::default()
-                                },
-                                extension: SceneBound { bounds: ui.bounds },
-                            },
-                            extension: TextQuad {
-                                data: material_data,
-                            },
-                        }),
-                        ..Default::default()
+                if let Ok(mut vis) = uis.get_mut(ui.ui_root) {
+                    *vis = Visibility::Visible;
+                }
+        
+                let image_size = Extent3d {
+                    width: if ui.resize_width.is_some() {
+                        16
+                    } else {
+                        ui.width.max(16)
                     },
-                    NotShadowCaster,
-                ))
-                .id();
-
-            commands.entity(ent).try_push_children(&[quad]);
-
-            (quad, image)
-        };
-
-        if let Some(mut commands) = commands.get_entity(ui.ui_root) {
-            cam.target = RenderTarget::Image(image.clone());
-            cam.is_active = true;
-            commands.insert(TargetCamera(cam_ent));
-            *current_rendered_ui = Some(ui.ui_root);
+                    height: if ui.resize_height.is_some() {
+                        16
+                    } else {
+                        ui.height.max(16)
+                    },
+                    depth_or_array_layers: 1,
+                };
+        
+                let material_data = TextQuadData {
+                    valign: ui.valign,
+                    halign: ui.halign,
+                    pix_per_m: ui.pix_per_m,
+                    add_y_pix: ui.add_y_pix,
+                };
+        
+                // update camera
+                cam.is_active = true;
+                target.width = ui.resize_width;
+                target.height = ui.resize_height;
+                target.info.max_width = Some(ui.width);
+                target.info.max_height = Some(ui.height);
+        
+                // create or update camera and quad
+                let (quad, image) = if let Some(prev_items) = wui.lookup.get(&ent) {
+                    if let Ok(quad) = quad_query.get(prev_items.quad) {
+                        if let Some(mat) = materials.get_mut(quad) {
+                            // update valign
+                            mat.extension.data = material_data;
+        
+                            // update image
+                            if let Some(image) =
+                                images.get_mut(mat.base.base.base_color_texture.clone().unwrap())
+                            {
+                                // let current_size = image.size();
+        
+                                // let width_ok =
+                                //     ui.resize_width.is_some() || image_size.width == current_size.x;
+                                // let height_ok =
+                                //     ui.resize_height.is_some() || image_size.height == current_size.y;
+                                // if !width_ok || !height_ok {
+                                image.resize(image_size);
+                                // }
+                            }
+                        }
+                    }
+        
+                    // dispose of previous ui if required
+                    if let Some(commands) = prev_items.ui.and_then(|e| commands.get_entity(e)) {
+                        commands.despawn_recursive();
+                    }
+        
+                    (prev_items.quad, prev_items.image.clone())
+                } else {
+                    // create render target image (it'll be resized)
+                    let mut image = Image::new_fill(
+                        image_size,
+                        TextureDimension::D2,
+                        &[0, 0, 0, 0],
+                        TextureFormat::Bgra8UnormSrgb,
+                    );
+                    image.texture_descriptor.usage |= TextureUsages::RENDER_ATTACHMENT;
+                    let image = images.add(image);
+        
+                    let quad = commands
+                        .spawn((
+                            MaterialMeshBundle {
+                                mesh: meshes.add(shape::Quad::default().into()),
+                                material: materials.add(TextShapeMaterial {
+                                    base: SceneMaterial {
+                                        base: StandardMaterial {
+                                            base_color_texture: Some(image.clone()),
+                                            unlit: true,
+                                            double_sided: true,
+                                            cull_mode: None,
+                                            alpha_mode: AlphaMode::Mask(0.5),
+                                            ..Default::default()
+                                        },
+                                        extension: SceneBound { bounds: ui.bounds },
+                                    },
+                                    extension: TextQuad {
+                                        data: material_data,
+                                    },
+                                }),
+                                ..Default::default()
+                            },
+                            NotShadowCaster,
+                        ))
+                        .id();
+        
+                    commands.entity(ent).try_push_children(&[quad]);
+        
+                    (quad, image)
+                };
+        
+                if let Some(mut commands) = commands.get_entity(ui.ui_root) {
+                    cam.target = RenderTarget::Image(image.clone());
+                    cam.is_active = true;
+                    commands.insert(TargetCamera(cam_ent));
+                    // wait 1 tick to ensure size is propagated correctly
+                    *current_rendered_ui = Some((ui.ui_root, 1));
+                }
+        
+                wui.lookup.insert(
+                    ent,
+                    WorldUiEntitySet {
+                        quad,
+                        image,
+                        ui: ui.dispose_ui.then_some(ui.ui_root),
+                    },
+                );
+        
+                commands.entity(ent).try_insert(ProcessedWorldUi);
+            }
         }
-
-        wui.lookup.insert(
-            ent,
-            WorldUiEntitySet {
-                quad,
-                image,
-                ui: ui.dispose_ui.then_some(ui.ui_root),
-            },
-        );
-
-        commands.entity(ent).try_insert(ProcessedWorldUi);
     }
 
     for (ent, wui) in new_uis {
