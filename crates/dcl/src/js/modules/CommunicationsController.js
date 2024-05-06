df10797d214e715525347dc1755a5ea4257cdf1a
robtfm <50659922+robtfm@users.noreply.github.com>
2024-05-06 02:16:24 +0100

    fix sendBinary

--- crates/dcl/src/js/modules/CommunicationsController.js
+++ crates/dcl/src/js/modules/CommunicationsController.js
@@ -5,8 +5,8 @@ module.exports.send = async function (body) {
 
 module.exports.sendBinary = async function (body) {
     // const data = (await Deno.core.ops.op_comms_send_binary([...body.data])).map((item) => new Uint8Array(item));
-    for (const buffer in body.data) {
-        await Deno.core.ops.op_comms_send_binary_single([...body.data])
+    for (const buffer of body.data) {
+        await Deno.core.ops.op_comms_send_binary_single(new Uint8Array(buffer));
     }
 
     const data = (await Deno.core.ops.op_comms_recv_binary()).map((item) => new Uint8Array(item));
