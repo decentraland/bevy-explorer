6c9d340b612549c3a4152423f37dde2a8b441d5d
robtfm <50659922+robtfm@users.noreply.github.com>
2024-05-17 14:18:31 +0100

    bump deno and wgpu

--- crates/dcl/src/js/fetch/fetch_response_body_resource.rs
+++ crates/dcl/src/js/fetch/fetch_response_body_resource.rs
@@ -2,57 +2,7 @@
 
 use std::{borrow::Cow, rc::Rc};
 
-use deno_core::{
-    error::type_error, AsyncRefCell, AsyncResult, BufView, CancelFuture, CancelHandle, RcRef,
-    WriteOutcome,
-};
-
-pub struct FetchRequestBodyResource {
-    pub body: AsyncRefCell<tokio::sync::mpsc::Sender<Option<bytes::Bytes>>>,
-    pub cancel: CancelHandle,
-}
-
-impl deno_core::Resource for FetchRequestBodyResource {
-    fn name(&self) -> Cow<str> {
-        "fetchRequestBody".into()
-    }
-
-    fn write(self: Rc<Self>, buf: BufView) -> AsyncResult<WriteOutcome> {
-        Box::pin(async move {
-            let bytes: bytes::Bytes = buf.into();
-            let nwritten = bytes.len();
-            let body = RcRef::map(&self, |r| &r.body).borrow_mut().await;
-            let cancel = RcRef::map(self, |r| &r.cancel);
-            body.send(Some(bytes))
-                .or_cancel(cancel)
-                .await?
-                .map_err(|_| type_error("request body receiver not connected (request closed)"))?;
-            Ok(WriteOutcome::Full { nwritten })
-        })
-    }
-
-    fn shutdown(self: Rc<Self>) -> AsyncResult<()> {
-        Box::pin(async move {
-            let body = RcRef::map(&self, |r| &r.body).borrow_mut().await;
-            let cancel = RcRef::map(self, |r| &r.cancel);
-            // There is a case where hyper knows the size of the response body up
-            // front (through content-length header on the resp), where it will drop
-            // the body once that content length has been reached, regardless of if
-            // the stream is complete or not. This is expected behaviour, but it means
-            // that if you stream a body with an up front known size (eg a Blob),
-            // explicit shutdown can never succeed because the body (and by extension
-            // the receiver) will have dropped by the time we try to shutdown. As such
-            // we ignore if the receiver is closed, because we know that the request
-            // is complete in good health in that case.
-            body.send(None).or_cancel(cancel).await?.ok();
-            Ok(())
-        })
-    }
-
-    fn close(self: Rc<Self>) {
-        self.cancel.cancel()
-    }
-}
+use deno_core::{AsyncRefCell, AsyncResult, BufView, CancelHandle, RcRef};
 
 // cheat and read the whole thing
 pub struct FetchResponseBodyResource {
