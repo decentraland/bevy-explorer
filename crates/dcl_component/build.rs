a5ab0e29a28c06bda1553e414b3059d276d77063
robtfm <50659922+robtfm@users.noreply.github.com>
2025-02-17 15:19:03 +0000

    ci

--- crates/dcl_component/build.rs
+++ crates/dcl_component/build.rs
@@ -68,7 +68,10 @@ fn gen_sdk_components() -> Result<()> {
 
     let mut config = prost_build::Config::new();
     for component in serde_components {
-        config.type_attribute(component, "#[derive(serde::Serialize, serde::Deserialize)]\n#[serde(rename_all = \"camelCase\")]");
+        config.type_attribute(
+            component,
+            "#[derive(serde::Serialize, serde::Deserialize)]\n#[serde(rename_all = \"camelCase\")]",
+        );
     }
 
     config.compile_protos(&sources, &["src/proto/"])?;
