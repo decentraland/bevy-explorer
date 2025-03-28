440f641c7510ad9b15f99ebc091787a934a46a85
robtfm <50659922+robtfm@users.noreply.github.com>
2025-03-28 14:29:06 +0000

    remove combobox debug

--- crates/ui_core/src/combo_box.rs
+++ crates/ui_core/src/combo_box.rs
@@ -192,7 +192,6 @@ fn update_comboboxen(
                       combo: Query<(&ComboBox, &Node, &GlobalTransform)>,
                       target_camera: TargetCameraHelper,
                       dui: Res<DuiRegistry>| {
-                    println!("click");
                     let Ok((cbox, node, gt)) = combo.get(ent) else {
                         warn!("no node");
                         return;
