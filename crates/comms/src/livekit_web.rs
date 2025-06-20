fdf3af591fe230c6b1010de8b8e5ddb988bf89dc
robtfm <50659922+robtfm@users.noreply.github.com>
2025-06-20 13:29:22 +0100

    fix wasm+livekit

--- crates/comms/src/livekit_web.rs
+++ crates/comms/src/livekit_web.rs
@@ -144,10 +144,11 @@ fn livekit_handler_inner(
         url.host().unwrap_or_default(),
         url.path()
     );
-    let params = HashMap::from_iter(url.query().unwrap_or_default().split('&').flat_map(|par| {
-        par.split_once('=')
-            .map(|(a, b)| (a.to_owned(), b.to_owned()))
-    }));
+    let params: HashMap<_, _, FixedHasher> =
+        HashMap::from_iter(url.query().unwrap_or_default().split('&').flat_map(|par| {
+            par.split_once('=')
+                .map(|(a, b)| (a.to_owned(), b.to_owned()))
+        }));
     debug!("{:?}", params);
     let token = params.get("access_token").cloned().unwrap_or_default();
 
