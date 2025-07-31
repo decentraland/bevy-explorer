70ac059410660b563ef02955e0640292d4de402a
robtfm <50659922+robtfm@users.noreply.github.com>
2025-07-31 23:27:59 +0100

    script comments

--- deploy/web-build/build_playground.sh
+++ deploy/web-build/build_playground.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
+
+set -e
+
+echo "--- installing playground node dependencies ---"
 npm i
+echo "--- running playground build ---"
 npm run build
+echo "--- copying playground artifacts ---"
 cp -r dist/* ../web
+echo "--- generating node modules for snapshot ---"
 mkdir dcl-deps
 cd dcl-deps
+echo "--- setting snapshot requirements ---"
 npm init -y
 npm pkg set overrides.esbuild="npm:esbuild-wasm@^0.25.8"
+echo "--- installing snapshot requirements ---"
 npm i @dcl/sdk@protocol-squad @dcl-sdk/utils @dcl/js-runtime esbuild-wasm
+echo "--- resolving symlinks ---"
 cp -rL node_modules node_modules_full
 cd ..
+echo "--- creating snapshot ---"
 node create-snapshot.mjs
+echo "--- playground build done ---"
