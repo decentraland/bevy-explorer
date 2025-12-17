37bbb49b9e5a2a4ce237dffe911d56a8ee9d6e4e
Lucas Farias <lucasfnf@gmail.com>
2025-12-17 14:44:05 -0300

    Fix struct name

--- src/main.rs
+++ src/main.rs
@@ -466,7 +466,7 @@ fn main() {
         app.add_plugins(AutomaticTestingPlugin);
     }
 
-    app.add_plugins(AudioPlugin)
+    app.add_plugins(AVPlayerPlugin)
         .add_plugins(RestrictedActionsPlugin)
         .insert_resource(PrimaryPlayerRes(Entity::PLACEHOLDER))
         .insert_resource(PrimaryCameraRes(Entity::PLACEHOLDER))
