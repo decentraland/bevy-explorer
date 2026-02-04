e5d2d23385bb3d38cec2519ee839e12d036173c7
robtfm <50659922+robtfm@users.noreply.github.com>
2026-02-04 15:21:46 +0000

    change ci video to google's chrome test vid

--- crates/av/src/test.rs
+++ crates/av/src/test.rs
@@ -7,7 +7,7 @@ use crate::video_context::VideoContext;
 #[cfg(feature = "ffmpeg")]
 #[test]
 fn test_ffmpeg() {
-    let context = input(&"https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875".to_owned()).unwrap();
+    let context = input(&"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4".to_owned()).unwrap();
     let (sx, _rx) = tokio::sync::mpsc::channel(1);
     VideoContext::init(&context, sx).unwrap();
 }
