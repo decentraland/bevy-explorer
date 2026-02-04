f7aaf02db0b57d632f5095024a1086730915f772
robtfm <50659922+robtfm@users.noreply.github.com>
2026-02-04 16:14:43 +0000

    ci

--- crates/av/src/test.rs
+++ crates/av/src/test.rs
@@ -7,7 +7,11 @@ use crate::video_context::VideoContext;
 #[cfg(feature = "ffmpeg")]
 #[test]
 fn test_ffmpeg() {
-    let context = input(&"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4".to_owned()).unwrap();
+    let context = input(
+        &"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            .to_owned(),
+    )
+    .unwrap();
     let (sx, _rx) = tokio::sync::mpsc::channel(1);
     VideoContext::init(&context, sx).unwrap();
 }
