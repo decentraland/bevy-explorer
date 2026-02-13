7ba3fb64f8f2b7f15c6023e8b9673a60605b4f01
Lucas Farias <lucasfnf@gmail.com>
2026-02-13 10:21:43 -0300

    Implement `SkyboxTime`

--- crates/visuals/src/day_night.rs
+++ crates/visuals/src/day_night.rs
@@ -1,7 +1,17 @@
-use bevy::{ecs::entity::EntityHashSet, prelude::*};
+use bevy::{
+    ecs::{component::ComponentIdFor, entity::EntityHashSet},
+    prelude::*,
+};
 use bevy_console::{AddConsoleCommand, ConsoleCommand};
 use common::structs::{PrimaryPlayerRes, SceneTime, TimeOfDay};
-use scene_runner::{renderer_context::RendererSceneContext, ContainingScene};
+use dcl::interface::ComponentPosition;
+use dcl_component::{
+    proto_components::sdk::components::{PbSkyboxTime, TransitionMode},
+    SceneComponentId,
+};
+use scene_runner::{
+    renderer_context::RendererSceneContext, update_world::AddCrdtInterfaceExt, ContainingScene,
+};
 
 /// One hour in seconds
 const ONE_HOUR: f32 = 60. * 60.;
@@ -33,15 +43,36 @@ struct TimeSkip {
 #[derive(Component)]
 struct SceneTimeSource(Entity);
 
+#[derive(Component, Deref)]
+#[component(immutable)]
+struct SkyboxTime(PbSkyboxTime);
+
+impl From<PbSkyboxTime> for SkyboxTime {
+    fn from(value: PbSkyboxTime) -> Self {
+        Self(value)
+    }
+}
+
+#[derive(Component)]
+struct SkyboxTimeSource(Entity);
+
 pub struct DayNightPlugin;
 
 impl Plugin for DayNightPlugin {
     fn build(&self, app: &mut App) {
+        app.add_crdt_lww_component::<PbSkyboxTime, SkyboxTime>(
+            SceneComponentId::SKYBOX_TIME,
+            ComponentPosition::RootOnly,
+        );
+
         app.add_systems(Startup, start_clock);
         app.add_systems(
             First,
             (
-                (update_running_clock, fetch_time_from_scene),
+                (
+                    update_running_clock,
+                    (fetch_time_from_skybox, fetch_time_from_scene).chain(),
+                ),
                 push_time_of_day_from_time_skip.run_if(resource_exists::<TimeSkip>),
                 push_time_of_day_from_running_clock.run_if(not(resource_exists::<TimeSkip>)),
             )
@@ -62,7 +93,8 @@ fn start_clock(mut commands: Commands) {
             RunningClock { time, speed: 12.0 },
             SceneTime { time: 0. },
         ))
-        .observe(check_new_scene_time);
+        .observe(check_new_scene_time)
+        .observe(check_new_skybox_time);
 }
 
 #[expect(clippy::type_complexity, reason = "Queries are complex")]
@@ -106,15 +138,59 @@ fn fetch_time_from_scene(
     }
 }
 
+#[expect(clippy::type_complexity, reason = "Queries are complex")]
+fn fetch_time_from_skybox(
+    mut commands: Commands,
+    primary_player: Res<PrimaryPlayerRes>,
+    containing_scene: ContainingScene,
+    scenes: Query<(Entity, &RendererSceneContext, &SkyboxTime)>,
+    time_keeper: Single<(Entity, Option<&SkyboxTime>, Option<&SkyboxTimeSource>), With<TimeKeeper>>,
+    scene_time_component_id: ComponentIdFor<SceneTime>,
+) {
+    let containing_primary_player = containing_scene.get(primary_player.0);
+    let maybe_skybox_time = scenes
+        .iter_many_unique(EntityHashSet::from_iter(containing_primary_player))
+        .min_by_key(|(_, renderer_scene_context, _)| {
+            (
+                renderer_scene_context.is_portable,
+                renderer_scene_context.start_tick,
+            )
+        })
+        .map(|(entity, _, skybox_time)| (entity, skybox_time));
+
+    let (time_keeper, time_keeper_skybox_time, skybox_time_source) = time_keeper.into_inner();
+    if let Some((entity, skybox_time)) = maybe_skybox_time {
+        if skybox_time_source
+            .filter(|source| source.0 == entity)
+            .is_none()
+        {
+            debug!("Now using skybox time from scene {}.", entity);
+            commands.entity(time_keeper).insert((
+                SkyboxTime(PbSkyboxTime {
+                    fixed_time: skybox_time.fixed_time,
+                    transition_mode: skybox_time.transition_mode,
+                }),
+                SkyboxTimeSource(entity),
+            ));
+        }
+    } else if time_keeper_skybox_time.is_some() {
+        debug!("No longer using SkyboxTime.");
+        commands
+            .entity(time_keeper)
+            .try_remove::<(SkyboxTime, SkyboxTimeSource)>();
+        commands.trigger_targets(OnInsert, (time_keeper, *scene_time_component_id));
+    }
+}
+
 fn update_running_clock(
     mut time_keeper: Single<&mut RunningClock, With<TimeKeeper>>,
     time: Res<Time>,
 ) {
     let speed = time_keeper.speed;
     time_keeper.time += time.delta_secs() * speed;
-    time_keeper.time %= 3600.0 * 24.0;
+    time_keeper.time %= TWENTY_FOUR_HOURS;
     if time_keeper.time < 0.0 {
-        time_keeper.time += 3600.0 * 24.0;
+        time_keeper.time += TWENTY_FOUR_HOURS;
     }
 }
 
@@ -133,7 +209,7 @@ fn push_time_of_day_from_time_skip(
     time_of_day.time = time_skip.start.lerp(
         time_skip.end,
         time_skip.easing.sample_clamped(time_skip.progress),
-    );
+    ) % TWENTY_FOUR_HOURS;
 
     if time_skip.progress >= 1. {
         commands.remove_resource::<TimeSkip>();
@@ -152,7 +228,7 @@ fn push_time_of_day_from_running_clock(
 fn check_new_scene_time(
     _trigger: Trigger<OnInsert, SceneTime>,
     mut commands: Commands,
-    time_keeper: Single<&SceneTime, With<TimeKeeper>>,
+    time_keeper: Single<&SceneTime, (With<TimeKeeper>, Without<SkyboxTime>)>,
     mut time_of_day: ResMut<TimeOfDay>,
 ) {
     let scene_time = time_keeper.into_inner();
@@ -163,6 +239,34 @@ fn check_new_scene_time(
             scene_time.time
         } else {
             scene_time.time + TWENTY_FOUR_HOURS
+        };
+        commands.insert_resource(TimeSkip {
+            start: time_of_day.time,
+            end,
+            progress: 0.,
+            easing: EaseFunction::SmoothStep,
+        });
+    }
+}
+
+fn check_new_skybox_time(
+    _trigger: Trigger<OnInsert, SkyboxTime>,
+    mut commands: Commands,
+    time_keeper: Single<&SkyboxTime, With<TimeKeeper>>,
+    mut time_of_day: ResMut<TimeOfDay>,
+) {
+    let skybox_time = time_keeper.into_inner();
+    if (skybox_time.fixed_time as f32 - time_of_day.time).abs() < ONE_HOUR {
+        time_of_day.time = skybox_time.fixed_time as f32;
+    } else {
+        let end = match (
+            skybox_time.fixed_time as f32 > time_of_day.time,
+            skybox_time.transition_mode(),
+        ) {
+            (true, TransitionMode::TmForward) => skybox_time.fixed_time as f32,
+            (false, TransitionMode::TmForward) => skybox_time.fixed_time as f32 + TWENTY_FOUR_HOURS,
+            (true, TransitionMode::TmBackward) => skybox_time.fixed_time as f32 - TWENTY_FOUR_HOURS,
+            (false, TransitionMode::TmBackward) => skybox_time.fixed_time as f32,
         };
         commands.insert_resource(TimeSkip {
             start: time_of_day.time,
