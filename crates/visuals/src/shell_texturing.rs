65420d5200a79a7f23ab22e8541d032e8544d283
Lucas Farias <lucasfnf@gmail.com>
2026-03-03 17:25:03 -0300

    Add component to spawn shell texturing grass

--- crates/visuals/src/shell_texturing.rs
+++ crates/visuals/src/shell_texturing.rs
@@ -1,8 +1,23 @@
 use bevy::{
-    asset::{embedded_asset, embedded_path},
+    asset::{embedded_asset, embedded_path, weak_handle},
+    color::palettes,
+    pbr::NotShadowCaster,
     prelude::*,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    render::{
+        mesh::MeshTag,
+        render_resource::{AsBindGroup, ShaderRef},
+    },
 };
+
+const SHELL_TEXTURING_MESH: Handle<Mesh> = weak_handle!("75b4bc5b-7523-4d7c-a42f-d2ddb93ac169");
+const SHELL_TEXTURING_MATERIAL: Handle<ShellTexture> =
+    weak_handle!("18c8dd1e-081d-452a-9c00-327775a239ff");
+
+#[derive(Component)]
+#[require(Transform, Visibility)]
+pub struct ShellTextureGrass {
+    pub parcel: IVec2,
+}
 
 #[derive(Clone, Asset, TypePath, AsBindGroup)]
 pub struct ShellTexture {
@@ -38,12 +53,84 @@ impl Material for ShellTexture {
     }
 }
 
+#[derive(Clone, Copy)]
 pub(crate) struct ShellTexturingPlugin;
 
 impl Plugin for ShellTexturingPlugin {
     fn build(&self, app: &mut App) {
+        embedded_asset!(app, "shell_texturing.wgsl");
+
+        app.init_resource::<ShellTexturingGrassConfig>();
+
         app.add_plugins(MaterialPlugin::<ShellTexture>::default());
 
-        embedded_asset!(app, "shell_texturing.wgsl");
+        app.add_systems(Startup, setup_assets);
+        app.add_observer(new_shell_texture_grass);
     }
+}
+
+#[derive(Resource)]
+pub struct ShellTexturingGrassConfig {
+    pub layers: u32,
+    pub subdivisions: u32,
+    pub y_displacement: f32,
+}
+
+impl Default for ShellTexturingGrassConfig {
+    fn default() -> Self {
+        Self {
+            layers: 32,
+            subdivisions: 16,
+            y_displacement: 0.01,
+        }
+    }
+}
+
+fn setup_assets(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ShellTexture>>,
+    shell_texturing_config: Res<ShellTexturingGrassConfig>,
+) {
+    meshes.insert(
+        SHELL_TEXTURING_MESH.id(),
+        Plane3d::new(Vec3::Y, Vec2::splat(8.)).mesh().build(),
+    );
+    materials.insert(
+        SHELL_TEXTURING_MATERIAL.id(),
+        ShellTexture {
+            subdivisions: shell_texturing_config.subdivisions,
+            layers: shell_texturing_config.layers,
+            padding: Vec2::default(),
+            root_color: palettes::tailwind::LIME_600.into(),
+            tip_color: palettes::tailwind::LIME_800.into(),
+        },
+    );
+}
+
+fn new_shell_texture_grass(
+    trigger: Trigger<OnAdd, ShellTextureGrass>,
+    mut commands: Commands,
+    shell_texturing_grasses: Query<&ShellTextureGrass>,
+    shell_texturing_config: Res<ShellTexturingGrassConfig>,
+) {
+    let entity = trigger.target();
+    let Ok(shell_texturing_grass) = shell_texturing_grasses.get(entity) else {
+        unreachable!("Infallible query");
+    };
+
+    commands.entity(entity).with_children(|parent| {
+        for i in 0..shell_texturing_config.layers {
+            parent.spawn((
+                Mesh3d(SHELL_TEXTURING_MESH.clone()),
+                MeshMaterial3d(SHELL_TEXTURING_MATERIAL.clone()),
+                Transform::from_translation(Vec3::new(
+                    16. * shell_texturing_grass.parcel.x as f32 + 8.,
+                    -0.05 + (shell_texturing_config.y_displacement * i as f32),
+                    -(16. * shell_texturing_grass.parcel.y as f32) - 8.,
+                )),
+                MeshTag(i),
+                NotShadowCaster,
+            ));
+        }
+    });
 }
