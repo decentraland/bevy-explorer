6fa7f116bbbd50de92d7f50cf8b58bcbbcd6a7f4
Lucas Farias <lucasfnf@gmail.com>
2026-03-06 11:05:11 -0300

    Work on making the logic to spawn parcel grass

--- crates/visuals/src/shell_texturing.rs
+++ crates/visuals/src/shell_texturing.rs
@@ -1,13 +1,19 @@
 use bevy::{
     asset::{embedded_asset, embedded_path, weak_handle},
+    ecs::{component::HookContext, world::DeferredWorld},
     pbr::NotShadowCaster,
+    platform::collections::HashMap,
     prelude::*,
     render::{
         mesh::MeshTag,
         render_resource::{AsBindGroup, ShaderRef},
     },
 };
-use common::structs::ParcelGrassConfig;
+use common::structs::{ParcelGrassConfig, PrimaryUser};
+use scene_runner::{
+    initialize_scene::{PointerResult, ScenePointers},
+    vec3_to_parcel,
+};
 
 const PARCEL_GRASS_MESH: Handle<Mesh> = weak_handle!("75b4bc5b-7523-4d7c-a42f-d2ddb93ac169");
 const PARCEL_GRASS_MATERIAL: Handle<ShellTexture> =
@@ -18,16 +24,40 @@ const IN_SCENE_PARCEL_GRASS_MATERIAL: Handle<ShellTexture> =
 const IN_SCENE_PARCEL_GRASS_LAYERS: u32 = 5;
 const IN_SCENE_PARCEL_GRASS_DISPLACEMENT: f32 = 0.01;
 
+#[derive(Default, Resource, Deref, DerefMut)]
+struct ParcelGrassMap(HashMap<IVec2, Entity>);
+
 #[derive(Component)]
 #[require(Transform, Visibility, ParcelGrassLod = ParcelGrassLod::High)]
+#[component(on_insert = Self::on_insert, on_replace = Self::on_replace)]
 pub struct ParcelGrass {
     pub parcel: IVec2,
+}
+
+impl ParcelGrass {
+    fn on_insert(mut deferred_world: DeferredWorld, hook_context: HookContext) {
+        let entity = hook_context.entity;
+        let parcel = deferred_world.get::<ParcelGrass>(entity).unwrap().parcel;
+
+        let mut parcel_grass_map = deferred_world.resource_mut::<ParcelGrassMap>();
+        parcel_grass_map.insert(parcel, entity);
+    }
+
+    fn on_replace(mut deferred_world: DeferredWorld, hook_context: HookContext) {
+        let entity = hook_context.entity;
+        let parcel = deferred_world.get::<ParcelGrass>(entity).unwrap().parcel;
+
+        let mut parcel_grass_map = deferred_world.resource_mut::<ParcelGrassMap>();
+        let removed_entity = parcel_grass_map.remove(&parcel).unwrap();
+        assert_eq!(entity, removed_entity);
+    }
 }
 
 #[derive(Clone, Copy, Component)]
 #[component(immutable)]
 #[repr(u8)]
 pub enum ParcelGrassLod {
+    Off = 0,
     High = 1,
     Mid = 2,
     Low = 3,
@@ -39,6 +69,9 @@ pub struct ParcelGrassShell;
 
 #[derive(Clone, Copy, Component)]
 struct NeedsParcelGrass;
+
+#[derive(Clone, Copy, Component)]
+struct ParcelGrassWaitingScenePointer;
 
 #[derive(Clone, Asset, TypePath, AsBindGroup)]
 pub struct ShellTexture {
@@ -81,6 +114,7 @@ impl Plugin for ShellTexturingPlugin {
     fn build(&self, app: &mut App) {
         embedded_asset!(app, "shell_texturing.wgsl");
 
+        app.init_resource::<ParcelGrassMap>();
         app.init_resource::<ParcelGrassConfig>();
 
         app.add_plugins(MaterialPlugin::<ShellTexture>::default());
@@ -93,7 +127,19 @@ impl Plugin for ShellTexturingPlugin {
         );
         app.add_systems(
             Update,
-            rebuild_parcel_grasses.after(parcel_grass_config_updated),
+            (parcel_grass_waiting_scene_pointer, rebuild_parcel_grasses)
+                .chain()
+                .after(parcel_grass_config_updated),
+        );
+        app.add_systems(
+            PreUpdate,
+            (fill_parcel_grass, drop_far_parcel_grass).run_if(player_changed_parcels),
+        );
+        app.add_systems(
+            PreUpdate,
+            (clear_parcel_grass_on_realm_change)
+                .run_if(resource_exists_and_changed::<CurrentRealm>)
+                .before(fill_parcel_grass),
         );
         app.add_observer(parcel_grass_lod_change);
     }
@@ -165,6 +211,10 @@ fn rebuild_parcel_grasses(
         commands.entity(entity).despawn_related::<Children>();
 
         let (lod, layers, displacement, material) = match parcel_grass_lod {
+            ParcelGrassLod::Off => {
+                commands.entity(entity).remove::<NeedsParcelGrass>();
+                continue;
+            }
             ParcelGrassLod::Low => (
                 3,
                 parcel_grass_config.layers,
@@ -214,5 +264,105 @@ fn rebuild_parcel_grasses(
                 }
             })
             .remove::<NeedsParcelGrass>();
+    }
+}
+
+fn parcel_grass_waiting_scene_pointer(
+    mut commands: Commands,
+    parcel_grasses: Populated<(Entity, &ParcelGrass), With<ParcelGrassWaitingScenePointer>>,
+    player: Single<&GlobalTransform, With<PrimaryUser>>,
+    scene_pointers: Res<ScenePointers>,
+) {
+    let parcel = vec3_to_parcel(player.translation());
+
+    for (entity, parcel_grass) in parcel_grasses.into_inner() {
+        match scene_pointers.get(parcel_grass.parcel) {
+            Some(PointerResult::Nothing) => {
+                let distance = parcel.distance_squared(parcel_grass.parcel);
+                // TODO: make this depend of the render distance
+                let lod = match distance {
+                    ..0 => ParcelGrassLod::InScene,
+                    0..8 => ParcelGrassLod::High,
+                    8..16 => ParcelGrassLod::Mid,
+                    16.. => ParcelGrassLod::Low,
+                };
+                commands
+                    .entity(entity)
+                    .insert((NeedsParcelGrass, lod))
+                    .remove::<ParcelGrassWaitingScenePointer>();
+            }
+            Some(PointerResult::Exists { .. }) => {
+                commands
+                    .entity(entity)
+                    .insert((NeedsParcelGrass, ParcelGrassLod::InScene))
+                    .remove::<ParcelGrassWaitingScenePointer>();
+            }
+            None => {}
+        }
+    }
+}
+
+fn player_changed_parcels(
+    player: Single<&GlobalTransform, With<PrimaryUser>>,
+    mut last_player_parcel: Local<Option<IVec2>>,
+) -> bool {
+    let current_parcel = vec3_to_parcel(player.translation());
+    let old_parcel = last_player_parcel.replace(current_parcel);
+    Some(current_parcel) != old_parcel
+}
+
+fn fill_parcel_grass(
+    mut commands: Commands,
+    player: Single<&GlobalTransform, With<PrimaryUser>>,
+    parcel_grass_map: Res<ParcelGrassMap>,
+) {
+    let parcel = vec3_to_parcel(player.translation());
+
+    // TODO: make this depend of the render distance
+    for i in -7i32..=7 {
+        let j_range = 7 - i.abs();
+        for j in -j_range..=j_range {
+            let parcel = parcel + IVec2::new(i, j);
+            if !parcel_grass_map.contains_key(&parcel) {
+                debug!(
+                    target: "visuals::parcel_grass::fill",
+                    "Creating parcel grass on parcel {parcel}."
+                );
+                commands.spawn((
+                    ParcelGrass { parcel },
+                    ParcelGrassLod::Off,
+                    ParcelGrassWaitingScenePointer,
+                ));
+            }
+        }
+    }
+}
+
+fn drop_far_parcel_grass(
+    mut commands: Commands,
+    player: Single<&GlobalTransform, With<PrimaryUser>>,
+    parcel_grass_map: Res<ParcelGrassMap>,
+) {
+    let parcel = vec3_to_parcel(player.translation());
+
+    for (parcel_grass, entity) in parcel_grass_map.iter() {
+        // TODO: make this depend of the render distance
+        if parcel.distance_squared(*parcel_grass) > 150 {
+            debug!(
+                target: "visuals::parcel_grass::drop_far",
+                "Dropping parcel grass for {} for being too far.",
+                *parcel_grass
+            );
+            commands.entity(*entity).despawn();
+        }
+    }
+}
+
+fn clear_parcel_grass_on_realm_change(
+    mut commands: Commands,
+    parcel_grass: Query<Entity, With<ParcelGrass>>,
+) {
+    for entity in parcel_grass {
+        commands.entity(entity).despawn();
     }
 }
