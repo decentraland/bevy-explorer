438770a28cf17821a0baab1c4c9f8e906b33f74a
Lucas Farias <lucasfnf@gmail.com>
2026-03-23 17:15:37 -0300

    Improve `AssetReaderError` returned by Ipfs

--- crates/scene_runner/src/asset_preload/plugin.rs
+++ crates/scene_runner/src/asset_preload/plugin.rs
@@ -1,4 +1,4 @@
-use std::{convert::Infallible, io::ErrorKind, path::PathBuf};
+use std::{convert::Infallible, path::PathBuf};
 
 use bevy::{
     asset::{
@@ -232,14 +232,8 @@ fn verify_preload_state(
                     AssetLoadError::AssetReaderError(AssetReaderError::NotFound(_)) => {
                         LoadingState::NotFound
                     }
-                    AssetLoadError::AssetReaderError(AssetReaderError::Io(io)) => {
-                        let message = io.to_string();
-                        if io.kind() == ErrorKind::Other && message.starts_with("w: file not found")
-                        {
-                            LoadingState::NotFound
-                        } else {
-                            LoadingState::FinishedWithError
-                        }
+                    AssetLoadError::AssetReaderError(AssetReaderError::Io(_)) => {
+                        LoadingState::FinishedWithError
                     }
                     _ => LoadingState::FinishedWithError,
                 };
