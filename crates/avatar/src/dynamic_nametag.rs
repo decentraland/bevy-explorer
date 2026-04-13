b3f604ee5056f4d5c619de1d610ee354b5de5353
Lucas Farias <lucasfnf@gmail.com>
2026-04-13 11:21:45 -0300

    Smooth out nametag height

--- crates/avatar/src/dynamic_nametag.rs
+++ crates/avatar/src/dynamic_nametag.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use bevy::{math::FloatOrd, prelude::*, render::primitives::Aabb};
 use common::structs::AttachPoints;
 use scene_runner::update_world::transform_and_parent::PostUpdateSets;
@@ -8,16 +10,57 @@ impl Plugin for DynamicNametagPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             PostUpdate,
-            dynamic_nametag_position
+            (
+                drop_values_too_old,
+                dynamic_nametag_position,
+                insert_current_frame_value,
+                smooth_out_height,
+            )
+                .chain()
                 .after(PostUpdateSets::PlayerUpdate)
                 .before(PostUpdateSets::AttachSync),
         );
+
+        app.add_observer(add_nametag_height_history);
+    }
+}
+
+#[derive(Default, Component)]
+struct NametagHeightHistory {
+    timestamps: VecDeque<f32>,
+    heights: VecDeque<f32>,
+    max: usize,
+}
+
+fn drop_values_too_old(nametags: Query<&mut NametagHeightHistory>, time: Res<Time<Real>>) {
+    let threshold = time.elapsed_secs_wrapped() - 0.25;
+    for mut nametag_height_history in nametags {
+        while nametag_height_history
+            .timestamps
+            .front()
+            .filter(|front| *front < &threshold)
+            .is_some()
+        {
+            nametag_height_history.timestamps.pop_front();
+            nametag_height_history.heights.pop_front();
+            if nametag_height_history.max == 0 {
+                let (max, _) = nametag_height_history
+                    .heights
+                    .iter()
+                    .enumerate()
+                    .max_by_key(|(_, height)| FloatOrd(**height))
+                    .unwrap_or((usize::MAX, &0.));
+                nametag_height_history.max = max;
+            } else {
+                nametag_height_history.max -= 1;
+            }
+        }
     }
 }
 
 fn dynamic_nametag_position(
     attach_points_query: Query<&AttachPoints>,
-    mut transforms: Query<&mut Transform>,
+    mut nametags: Query<&mut Transform>,
     global_transforms: Query<&GlobalTransform>,
     aabbs: Query<&Aabb>,
 ) {
@@ -43,7 +86,7 @@ fn dynamic_nametag_position(
             unreachable!("List is never empty.");
         };
 
-        let Ok(mut nametag_transform) = transforms.get_mut(attach_points.nametag) else {
+        let Ok(mut nametag_transform) = nametags.get_mut(attach_points.nametag) else {
             panic!("Nametag must have Transform.");
         };
         let position_rotation = {
@@ -57,6 +100,57 @@ fn dynamic_nametag_position(
                 head_position.translation.z - position.translation.z,
             );
     }
+}
+
+fn insert_current_frame_value(
+    nametags: Query<(&Transform, &mut NametagHeightHistory)>,
+    time: Res<Time<Real>>,
+) {
+    for (transform, mut nametag_height_history) in nametags {
+        let index = nametag_height_history.heights.len();
+        let new_height = transform.translation.y;
+        let old_height = nametag_height_history
+            .heights
+            .get(nametag_height_history.max)
+            .copied()
+            .unwrap_or(0.);
+
+        nametag_height_history.heights.push_back(new_height);
+        nametag_height_history
+            .timestamps
+            .push_back(time.elapsed_secs_wrapped());
+        if new_height > old_height || nametag_height_history.max > index {
+            nametag_height_history.max = index;
+        }
+    }
+}
+
+fn smooth_out_height(nametags: Query<(&mut Transform, &NametagHeightHistory)>) {
+    for (mut transform, nametag_height_history) in nametags {
+        if let Some(max) = nametag_height_history
+            .heights
+            .get(nametag_height_history.max)
+            .filter(|max| **max > transform.translation.y)
+        {
+            transform.translation.y = *max;
+        }
+    }
+}
+
+fn add_nametag_height_history(
+    trigger: Trigger<OnAdd, AttachPoints>,
+    mut commands: Commands,
+    attach_points_query: Query<&AttachPoints>,
+) {
+    let entity = trigger.target();
+
+    let Ok(attach_points) = attach_points_query.get(entity) else {
+        unreachable!("Infallible query.");
+    };
+
+    commands
+        .entity(attach_points.nametag)
+        .insert(NametagHeightHistory::default());
 }
 
 fn nametag_offset(
