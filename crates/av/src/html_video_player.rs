cbcf6fccba3255220a9b5d47a2517e92e436ed9b
Lucas Farias <lucasfnf@gmail.com>
2026-08-04 18:25:24 -0300

    Revert "Remove stream `HtmlMediaEntity`"
    This reverts commit e45816a58da01bd1758c264e74d52d7647d13ef4.

--- crates/av/src/html_video_player.rs
+++ crates/av/src/html_video_player.rs
@@ -330,6 +330,81 @@ impl<T: AVPlayer> HtmlMediaEntity<T> {
         slf
     }
 
+    pub fn new_stream(source: String, image: Handle<Image>) -> Option<Self> {
+        let media = web_sys::window().unwrap().document().and_then(|doc| {
+            let container = doc
+                .get_element_by_id(STREAM_CONTAINER_ID)
+                .expect("streamer video container should exist");
+            let video = container
+                .get_elements_by_tag_name("video")
+                .get_with_index(0)?;
+            video.dyn_into::<HtmlMediaElement>().ok()
+        })?;
+
+        let video = media.clone().dyn_into::<HtmlVideoElement>().unwrap();
+
+        let frame_time = Arc::new(AtomicU32::default());
+
+        // video frame callback - no wasm_bindgen for this!
+        let rvc_prop = Reflect::get(&video, &"requestVideoFrameCallback".into()).unwrap();
+        if rvc_prop.is_undefined() {
+            panic!("no requestVideoFrameCallback");
+        }
+        let rvc_fn = rvc_prop.dyn_into::<web_sys::js_sys::Function>().unwrap();
+
+        let callback: RcClosure = Rc::new(RefCell::new(None));
+        let callback_handle: Rc<RefCell<Option<u32>>> = Rc::new(RefCell::new(None));
+        let callback_clone = callback.clone();
+        let handle_clone = callback_handle.clone();
+        let frame_time_clone = frame_time.clone();
+        let rvc_clone = rvc_fn.clone();
+
+        *callback.borrow_mut() = Some(Closure::wrap(Box::new({
+            let video = video.clone();
+            move |_now: f64, metadata: JsValue| {
+                trace!("stream frame received");
+                if let Some(media_time) = Reflect::get(&metadata, &"mediaTime".into())
+                    .ok()
+                    .and_then(|mt| mt.as_f64())
+                {
+                    trace!("stream frame received -> {media_time}");
+                    frame_time_clone.store(
+                        (media_time as f32).to_bits(),
+                        std::sync::atomic::Ordering::Relaxed,
+                    );
+                };
+
+                if let Some(cb) = callback_clone.borrow().as_ref() {
+                    if let Ok(new_handle) = rvc_clone.call1(&video, cb.as_ref().unchecked_ref()) {
+                        *handle_clone.borrow_mut() = new_handle.as_f64().map(|f| f as u32);
+                    }
+                } else {
+                    warn!("no stream cb - dropping");
+                }
+            }
+        }) as Box<dyn FnMut(f64, JsValue)>));
+        let initial_handle = rvc_fn
+            .call1(
+                &video,
+                callback.borrow().as_ref().unwrap().as_ref().unchecked_ref(),
+            )
+            .unwrap();
+        *callback_handle.borrow_mut() = initial_handle.as_f64().map(|f| f as u32);
+
+        let mut slf = Self::common_init(source, media);
+        slf.video = Some(video);
+        slf.image = Some(image);
+        slf.new_frame_time = frame_time;
+        slf.frame_closure = callback;
+        slf.frame_callback_handle = callback_handle;
+
+        // Hack to force a callback trigger
+        slf.stop();
+        slf.play();
+
+        Some(slf)
+    }
+
     pub fn new_noop(source: String, image: Handle<Image>) -> Self {
         let media = web_sys::window()
             .unwrap()
